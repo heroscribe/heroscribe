59c3ba41dfef77afe50c1ae8aa264e0c8dbf29bc
Your Name <you@example.com>
2025-04-07 16:35:52 +0200

    cmpaigns.js

---
+++ campaigns.js
@@ -0,0 +1,144 @@
+
+gs_us = [
+  "01-TheTrial", "02-TheRescueofSirRagnar", "03-LairoftheOrcWarlord",
+  "04-PrinceMagnusGold", "05-MelarsMaze", "06-LegacyoftheOrcWarlord", "07-TheLostWizard",
+  "08-TheFireMage", "09-RaceAgainstTime", "10-CastleofMystery", "11-BastionofChaos",
+  "12-BarakTor-BarrowoftheWitchLord", "13-QuestfortheSpiritBlade", "14-ReturntoBarakTor",
+];
+gs_eu = [
+  "00-TheMaze", "01-TheTrial", "02-TheRescueofSirRagnar", "03-LairoftheOrcWarlord",
+  "04-PrinceMagnusGold", "05-MelarsMaze", "06-LegacyoftheOrcWarlord", "07-TheStoneHunter",
+  "08-TheFireMage", "09-RaceAgainstTime", "10-CastleofMystery", "11-BastionofChaos",
+  "12-BarakTor-BarrowoftheWitchLord", "13-QuestfortheSpiritBlade", "14-ReturntoBarakTor",
+];
+kk = [
+  "01-TheGreatGate", "02-TheWarriorHalls", "03-TheSpiralPassage", "04-TheDwarvenForge",
+  "05-HallofDwarvenKings", "06-TheGreatCitadel", "07-TheEasternPassage", "08-BelornsMine",
+  "09-TheEastGate", "10-GrinsCrag",
+]
+rotwl = [
+  "01-TheGateofDoom", "02-TheColdHalls", "03-TheSilentPassages", "04-HallsofVision",
+  "05-TheGateofBellthor", "06-HallsoftheDead", "07-TheForgottenLegion",
+  "08-TheForbiddenCity", "09-TheLastGate", "10-TheCourtoftheWitchLord",
+]
+atoh = [
+   "01-SearchfortheOgreFortress", "02-TheOuterCaves", "03-LairoftheOgreHorde",
+   "04-TheCarrionHalls", "05-ThePitofChaos", "06-FortressoftheOgreLord",
+   "07-FighttotheSurface",
+]
+wom = [
+  "01-TheToweroftheHighMage", "02-CryptoftheNecromancer", "03-EyrieoftheStormMaster",
+  "04-LairoftheOrcShaman", "05-TheFinalConflict",
+]
+dc = [""]
+fh = [
+  "01-XanonPass", "02-TrialbyIce", "03-TheRescue", "04-TheGlacialGate", "05-TheDeadlyDepths",
+  "06-TheFrostedPath", "07-TheHallsofKelvinos", "08-TheSearchfortheScepter",
+  "09-TheHeartofIcePartI", "10-TheHeartofIcePartII",
+]
+mm = [
+  "01-TheAvengerReturns", "02-OnSacredGround", "03-TerelliasMaze", "04-TheElvenProspector",
+  "05-TheAlchemistsLaboratory", "06-TormuksGuests", "07-GlinessFen",
+  "08-TheGatheringStorm", "09-HiddenRealmsPartI", "10-HiddenRealmsPartII",
+]
+sq = [
+  "AGrowlofThunder", "RunningtheGauntlet",
+]
+custom = ["01-ANewBeginning", "02-CozyHome", "03-CarpetsforSale"]
+
+intro_gs_eu = `
+<p>The fire burns warmly, but casts little light into Mentor's study.
+The flickering shadows only hint at the vast number of books and scrolls
+that fill the many shelves. Slowly Mentor walks over to the fire.
+<br/>"Well, my friends, your training is complete. You are not yet
+Heroes, you have yet to prove yourselves. But first, let me tell you of
+Morcar........</p>
+<p>"Many centuries ago, Morcar was my apprentice. He worked hard
+and learned quickly. But impatience devoured him, and he wanted to learn
+more powerful magic. I told him of the dangers, and that he should be
+patient, for in time he would become a great wizard. But Morcar could
+not wait: each night he broke into my study and read my spell books. The
+secrets that were held within them were great indeed. Once he had learnt
+these secrets. Morcar fled.</p>
+<p>"When I caught up with him, I found him greatly changed. He had
+pledged his allegiance to the Great Powers of Chaos. Fool! He saw magic
+only as a short-cut to power and paid no heed to the terrible price he
+would have to pay. I tried to reason with him, but to no avail. He laughed
+in my face and then unleashed a terrible spell which I was hard-pressed to
+counter. For many days we battled. but Morcar had allies stronger even
+than I, and I could not defeat him. In the end, as we both weakened, he
+fled and sought refuge in the Northern Chaos Wastes. There he licked his
+wounds and honed his skills, conjuring ancient powers with which to
+overthrow the Empire. But he knows not what he has done. They will
+serve him while it suits their own dark purposes, but in time they will
+destroy him.</p>
+<p>"I must watch Morcar and measure the strength of his magic. This
+I may do with the help of Loretome. The powers Morcar has called
+upon will destroy us all if I relax from this vigil. Morcar's legions have
+threatened us once before. Then it was Rogar who aided me and defeated
+them. Now they are on the march again: already they have assailed the
+Borderlands. The Empire must again look for Heroes and to this end have
+I trained you.</p>
+<p>"Each of you must complete three tasks. If you do this, you will be
+acclaimed as Champions of the Realm and dubbed Imperial Knights.
+Then will you truly be on the road to becoming a Hero. I shall
+speak with you again on your return - if you return......."</p>
+`
+outro_gs_eu = `
+<p>"You have done well. And yet it seems your task is hardly begun.
+Dark clouds gather at the Empire's borders...</p>
+<p>"The Emperor rode with his army towards Black Fire Pass, that
+perilous mountain route linking the Empire and the Borderlands. No such
+army has ever been raised in living memory. Greater even than Rogar's
+host it was. They drew up their battle lines on the grassy plains at the
+foot of the mountain road and waited. 'Ere nightfall, the sky darkened and
+a great wind arose. Spine-chilling shrieks and howls echoed from the
+mountain tops. terrifying the horses and filling man and elf alike with
+fear.</p>
+<p>"Then came the first assault. Pouring down the mountainsive as
+though it were a gentle slope rather than an almost vertical cliff, the
+hordes of Morcar fell upon the Empire's defenders. Like a black wave they
+came: but the lines held and it seemed we should win the day.</p>
+<p>"But I had not reckoned with Morcar's magic. He stood high on a
+ridge overlooking the battlefield and unleashed his terrible magic.
+Lightning flashed from his fingertips and great gouts of flame exploded
+among the defenders. Our lines were breached in a dozen places at once and
+the enemy howled in delight.</p>
+<p>"Then came the Doomguard - Morcar's elite force of Chaos
+Knights. Our army broke and ran. Only the Emperor's personal guard held
+their ground and many sacrificed themselves to ensure his safe retreat.</p>
+<p>"The remnants of the army are now holed up in Karak Varn - the
+ancient dwarven stronghold. They do not have provisions for a long siege,
+however, so we must act swiftly if we are to save the Emperor and his
+army.</p>
+<p>"Yet there is another evil to be confronted. The Witch Lord lives.
+He too had magic I had not foreseen and was protected from the Spirit
+Blade. He was sorely wounded, however, and fled to his ancient retreat in
+the Black Mountains. But soon he will have recovered enough to lead the
+Legion of the Damned once more.</p>
+<p>"The Empire has great need of you. I myself cannot act against
+these dangers, for I must move against Morcar and strive to weaken his
+power. Until we meet again, take care, my friends. Farewell......."</p>
+`
+
+campaigns = {
+  "EU": {
+    "ADC": {name: "AdventureDesignKit", quests: custom},
+    "GS": {name: "HQBase", quests: gs_eu, intro: intro_gs_eu, outro: outro_gs_eu},
+    "KK": {name: "KellarsKeep", quests: kk},
+    "RotWL": {name: "ReturnOfTheWitchLord", quests: rotwl},
+    "AtOH": {name: "AgainstTheOgreHorde", quests: atoh},
+    "WoM": {name: "WizardsOfMorcar", quests: wom},
+    "DC": {name: "TheDarkCompany", quests: dc},
+    "SQ": {name: "SoloQuests", quests: sq},
+  },
+  "US": {
+    "ADC": {name: "AdventureDesignKit", quests: custom},
+    "GS": {name: "HQBase", quests: gs_us, intro: intro_gs_eu.replaceAll("Morcar", "Zargon") , outro: outro_gs_eu.replaceAll("Morcar", "Zargon")},
+    "KK": {name: "KellarsKeep", quests: kk},
+    "RotWL": {name: "ReturnOfTheWitchLord", quests: rotwl},
+    "FH": {name: "TheFrozenHorror", quests: fh},
+    "MM": {name: "TheMageOfTheMirror", quests: mm},
+  },
+}
+
